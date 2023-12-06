0590c6a7716dbf936399a6755888ac09441784e9
olekskhom <oleks.khom@gmail.com>
2023-12-06 17:31:22 +0300

    step_4

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('test')
+const contacts = require('./contacts')
+const { Command } = require('commander');
+
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+        case 'list':
+            console.table(await contacts.listContacts());
+            break;
+
+        case 'get':
+            console.table(await contacts.getContactById(id));
+            break;
+
+        case 'add':
+            console.table(await contacts.addContact(name, email, phone));
+            break;
+
+        case 'remove':
+            console.table(await contacts.removeContact(id));
+            break;
+
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+    }
+}
+
+invokeAction(argv);
